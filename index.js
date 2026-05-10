89d3a5a3d0f8fd7b6e11c06716b4789279c3fc1d
RV-Coder01 <rvcoder01@gmail.com>
2026-05-10 23:13:37 +0530

    base of js-1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 save=document.getElementById("save-btn");
 
 save.addEventListener("click",function(){
-    console.log("Button was clicked");
+    console.log("Button was clicked")
     console.log("ho")
 })
